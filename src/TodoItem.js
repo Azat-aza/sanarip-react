11c82bde49d5b998f564d080cbdec9abf876ed80
Graza <709507028aizada@gmail.com>
2022-09-23 13:21:12 +0300

    dfn

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,15 +1,10 @@
 import React from "react";
-import TodCoList from './TodoList'
-import './App.css'
 
 
-function TodCoList(){
+function Text({myFunktion}){
     return(
-         <div className="div-1"> 
-        <h1>PUMA</h1>
-        <h2>1000000.00001</h2>
-    </div>
+        <p onClick={myFunktion}>log</p>
     )
-   
 }
-export default TodoItem
+
+export default Text
